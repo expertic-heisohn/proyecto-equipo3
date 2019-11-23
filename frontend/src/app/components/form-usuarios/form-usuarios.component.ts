0a98ce4aa46ae3eb6d5c53ffae34ab01e3209c3c
Duvian <d.restrepo1@pascualbravo.edu.co>
2019-11-23 17:46:49 -0500

    se creo el componente form-usuarios

---
+++ frontend/src/app/components/form-usuarios/form-usuarios.component.ts
@@ -0,0 +1,10 @@
+import { Component, } from '@angular/core';
+
+@Component({
+  selector: 'app-form-usuarios',
+  templateUrl: './form-usuarios.component.html',
+})
+export class FormUsuariosComponent  {
+
+}
+
