1a8f71ecdfda43f1b6f8bedcc50a1d90dabf9d61
Duvian <d.restrepo1@pascualbravo.edu.co>
2019-11-23 18:39:41 -0500

    se crearon todos los campos del formulario usuario

--- frontend/src/app/components/form-usuarios/form-usuarios.component.ts
+++ frontend/src/app/components/form-usuarios/form-usuarios.component.ts
@@ -1,10 +1,13 @@
-import { Component, } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-usuarios',
   templateUrl: './form-usuarios.component.html',
 })
-export class FormUsuariosComponent  {
+export class FormUsuariosComponent {
 
 }
+
+
+
 
