9f2f5a3e1a3e175afee7d1d94a8fd09f58aad738
juliangranda <juliangranda@hotmail.com>
2019-11-23 21:12:49 -0500

    validaciones por DATA con nombre

--- frontend/src/app/components/form-usuarios/form-usuarios.component.ts
+++ frontend/src/app/components/form-usuarios/form-usuarios.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 
+import{FormGroup, FormControl, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-form-usuarios',
   templateUrl: './form-usuarios.component.html',
 })
-export class FormUsuariosComponent {
 
+export class FormUsuariosComponent {
+  formaUsuario:FormGroup;
+  constructor(){
+    this.formaUsuario = new FormGroup({
+      //valor varible default/validadores/validadores asincronos
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'apellido': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'correo': new FormControl('', [ Validators.required, 
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+    })
+  }
+
+  guardarCambios(){
+    console.log(this.formaUsuario);
+    /*
+    if (this.forma.valid) {
+      console.log(this.forma.value);
+      alert("excelente");
+
+    } else {
+      alert("llenar los campos obligatorios");
+    }
+    */
+  }
+  
+  
+  
 }
 
 
