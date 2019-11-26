6dba6674850a32c7b106916b7df1538cc0194634
juliangranda <juliangranda@hotmail.com>
2019-11-25 23:04:59 -0500

    arreglo de interaccion MDB, validadores empleador

--- frontend/src/app/components/form-empleador/form-empleador.component.ts
+++ frontend/src/app/components/form-empleador/form-empleador.component.ts
@@ -12,13 +12,13 @@ export class FormEmpleadorComponent {
   constructor() {
     this.formaEmpleador = new FormGroup({
       //valor varible default/validadores/validadores asincronos
-      'nombre_empresa': new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'digitodeverificacion': new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'correoEmpresa': new FormControl('', [ Validators.required, 
-                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
-      'numeroDocumentoEmpresa': new FormControl('',[Validators.required, Validators.min(1000000), Validators.max(10000000000)]),
+      'nombreEmpresa': new FormControl('',[Validators.required, Validators.minLength(3)]),
+      'digitoVerificacion': new FormControl('',[Validators.required, Validators.minLength(3)]),
       'telefonoEmpresa': new FormControl('',[Validators.required, Validators.min(1000000), Validators.max(9999999)]),
+      'correoEmpresa': new FormControl('',[Validators.required,
+                                           Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
       'direccionPrincipalEmpresa': new FormControl('', [Validators.required, Validators.minLength(10)]),
+      'nombreContacto': new FormControl('',[Validators.required,Validators.minLength(5)]),
       
     })
    }
