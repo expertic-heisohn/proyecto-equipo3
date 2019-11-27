4bde88d2ab7e650c9af3ea19924974957df325d4
juliangranda <juliangranda@hotmail.com>
2019-11-27 18:36:29 -0500

    creacion service usuarios e interfaz usuario

---
+++ frontend/src/app/services/usuarios.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import IUsuario from "../interfaces/usuario";
+import { Observable } from "rxjs";
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsuarios(): Observable<IUsuario[]> {
+    return this.http.get<IUsuario[]>(
+      "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios"
+    );
+  }
+}
